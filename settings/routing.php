b34048c1fd929029a4c64d03c0ce8212bbb6c140
Xavier Chopin <xchopin54@gmail.com>
2017-05-07 17:32:27 +0200

    Added route

--- settings/routing.php
+++ settings/routing.php
@@ -5,4 +5,6 @@ $app->GET('/', 'app.controller:home')->bind('home');
 // --- AUTHENTICATION ---
 $app->GET('/authentication', 'auth.controller:signin')->bind('authentication');
 $app->GET('/signup', 'auth.controller:signup')->bind('signup');
+
 $app->POST('/create-account', 'auth.controller:signup')->bind('create-account');
+$app->POST('/signin', 'auth.controller:signin')->bind('signin');
